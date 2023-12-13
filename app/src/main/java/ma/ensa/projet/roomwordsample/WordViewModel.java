37f7e06ab6342dd36e8220a33feed403d4a38164
nada-han <nada.hansal2001@gmail.com>
2023-12-13 21:54:25 +0100

    first commit

---
+++ app/src/main/java/ma/ensa/projet/roomwordsample/WordViewModel.java
@@ -0,0 +1,28 @@
+package ma.ensa.projet.roomwordsample;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+import ma.ensa.projet.roomwordsample.data.WordRepository;
+import ma.ensa.projet.roomwordsample.model.Word;
+
+public class WordViewModel extends AndroidViewModel {
+
+    private final WordRepository mRepository;
+
+    private final LiveData<List<Word>> mAllWords;
+
+    public WordViewModel (Application application) {
+        super(application);
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
+    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
+}
